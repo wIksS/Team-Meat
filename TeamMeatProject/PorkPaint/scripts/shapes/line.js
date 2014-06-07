75edc1b1bbaed4b244ee558921e8a881493e2e1b
Viktor <viktordakov97@gmail.com>
2014-06-07 17:03:21 +0300

    Added line functionality. Now you can click and draw a line. Added line.js file.

--- TeamMeatProject/PorkPaint/scripts/shapes/line.js
+++ TeamMeatProject/PorkPaint/scripts/shapes/line.js
@@ -1,1 +1,34 @@
-﻿
+﻿function Line(curStage,strokeColor,lineStrokeWidth) {
+    this.draw = function (initMousePosition) {
+        var layer = new Kinetic.Layer(),
+        isMouseUp = false,
+        curMousePosition,
+        line = new Kinetic.Line({
+            points: [],
+            stroke: strokeColor,
+            strokeWidth: lineStrokeWidth
+        });
+
+        layer.add(line);
+        curStage.add(layer);
+
+        $('#canvas-container').on('mousemove', function (e) {
+            curMousePosition = new Point(e.pageX, e.pageY);
+        });
+        $('body').on('mouseup', function () {
+            isMouseUp = true;
+        })
+
+        var anim = new Kinetic.Animation(function (frame) {
+            var time = frame.time,
+                timeDiff = frame.timeDiff,
+                frameRate = frame.frameRate;
+            line.setPoints([initMousePosition.x, initMousePosition.y, curMousePosition.x, curMousePosition.y]);
+            if (isMouseUp) {
+                anim.stop();
+            }
+        }, layer);
+
+        anim.start();
+    }
+}
---
+++ TeamMeatProject/PorkPaint/scripts/shapes/point.js
@@ -0,0 +1,4 @@
+﻿var Point = function (x, y) {
+    this.x = x;
+    this.y = y;
+}
