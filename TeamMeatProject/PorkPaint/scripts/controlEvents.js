e7183ee74c35ad861db252f5c2662c8e9a197d94
sttodorov <st.todorov9@gmail.com>
2014-06-13 01:54:25 +0300

    Separate Toolbox

---
+++ TeamMeatProject/PorkPaint/scripts/controlEvents.js
@@ -0,0 +1,90 @@
+﻿$('#strokeColor').on('click', function () {
+    eng.setColorFocus('stroke');
+    $('#strokeColor').attr({
+        fill: controlsDefaultSelectedBackgroundColor
+    });
+    $('#fillColor').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+});
+$('#fillColor').on('click', function () {
+    eng.setColorFocus('fill');
+    $('#fillColor').attr({
+        fill: controlsDefaultSelectedBackgroundColor
+    });
+    $('#strokeColor').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+});
+$('#fillColorInner').on('click', function () {
+    eng.setColorFocus('fill');
+    $('#fillColor').attr({
+        fill: controlsDefaultSelectedBackgroundColor
+    });
+    $('#strokeColor').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+});
+$('#strokeWidth1').on('click', function () {
+    $('#strokeWidth1').attr({
+        fill: controlsDefaultSelectedBackgroundColor
+    });
+    $('#strokeWidth3').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+    $('#strokeWidth5').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+    $('#strokeWidth10').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+});
+$('#strokeWidth3').on('click', function () {
+    $('#strokeWidth1').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+    $('#strokeWidth3').attr({
+        fill: controlsDefaultSelectedBackgroundColor
+    });
+    $('#strokeWidth5').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+    $('#strokeWidth10').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+});
+$('#strokeWidth5').on('click', function () {
+    $('#strokeWidth1').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+    $('#strokeWidth3').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+    $('#strokeWidth5').attr({
+        fill: controlsDefaultSelectedBackgroundColor
+    });
+    $('#strokeWidth10').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+});
+$('#strokeWidth10').on('click', function () {
+    $('#strokeWidth1').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+    $('#strokeWidth3').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+    $('#strokeWidth5').attr({
+        fill: controlsDefaultBackgroundColor
+    });
+    $('#strokeWidth10').attr({
+        fill: controlsDefaultSelectedBackgroundColor
+    });
+});
+$('#colorSelector').on('click', function () {
+    input.click();
+});
+$('#colorSelectorInner').on('click', function () {
+    input.click();
+});
+
---
+++ TeamMeatProject/PorkPaint/scripts/creatingControls.js
@@ -0,0 +1,32 @@
+﻿// Creates a new shape control
+function createShapeControl(name, shape, drawInnerPictureFunc) {
+    controlName = name;
+    createControl(currentControlX, currentControlY, controlName, drawInnerPictureFunc);
+    $('#' + controlName).on('click', function () {
+        eng.setShape(shape);
+    });
+}
+
+function createStrokeControl(name, strokeWidth, drawInnerPictureFunc) {
+    controlName = name;
+    createControl(currentControlX, currentControlY, controlName, drawInnerPictureFunc);
+    $('#' + controlName).on('click', function () {
+        eng.setOutlineWidth(strokeWidth);
+
+    });
+}
+
+// Draws a new control box and its inner picture if applicable
+function createControl(x, y, controlId, drawInnerPictureFunc) {
+    var control = drawControlBox(x, y);
+    control.node.id = controlId;
+
+    if (drawInnerPictureFunc) {
+        var innerPicture = drawInnerPictureFunc(x, y);
+        setDefaultAttributes(innerPicture);
+        return innerPicture;
+    } else {
+        moveCoordinatesToNextValidPosition();
+        return control;
+    }
+}
